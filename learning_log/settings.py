a4798b939e785a80a44067f4e2bf60ad633a52a9
Burxoniddin <burxon19990409@gmail.com>
2022-08-08 16:16:38 +0500

    static created

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -153,7 +153,9 @@ if cwd == '/app' or cwd[:4] == '/tmp':
 
     # Static asset configuration.
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIR = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
